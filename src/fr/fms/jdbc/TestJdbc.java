1938c029042db2bd4c6180fa6cf67aae258ffeeb
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-31 13:00:22 +0100

    Modify TestJdbc to include properties

--- src/fr/fms/jdbc/TestJdbc.java
+++ src/fr/fms/jdbc/TestJdbc.java
@@ -1,5 +1,8 @@
 package fr.fms.jdbc;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,29 +10,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Properties;
+
 import fr.fms.entities.Article;
 
 public class TestJdbc {
 
 	//Initialisation de la base de données
+	/*
 	private static String url = "jdbc:mariadb://localhost:3306/shop";
 	private static String login = "root";
 	private static String password = "fms2024";
+	*/
+	private static Properties prop;
 	
 	public static void main(String[] args) {
 		ArrayList<Article> articles = new ArrayList<Article>();
 		
 		//Appel du driver jdbc de mariaDB
 		try {
-			Class.forName("org.mariadb.jdbc.Driver");
+			prop = readPropertiesFile("config.properties");
+			Class.forName(prop.getProperty("db.driver.class"));
 		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		
 		
 		//Connexion à la base de données et affichage des articles
-		try(Connection connection = DriverManager.getConnection(url, login, password)) {
+		try(Connection connection = DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.login"), prop.getProperty("db.password"))) {
 			String strSql = "SELECT * FROM T_Articles";
 			try(Statement statement = connection.createStatement()){
 				try(ResultSet resultSet = statement.executeQuery(strSql)){
@@ -53,8 +64,8 @@ public class TestJdbc {
 				System.out.println(a.getIdArticle() + " - " + a.getDescription() + " - " + a.getBrand() + " - " + a.getUnitaryPrice());
 			}
 			
-			Article article = articles.get(7);
-			showInfoArticle(article);
+			//Article article = articles.get(7);
+			//showInfoArticle(article);
 			
 		}catch(SQLException e) {
 			e.printStackTrace();
@@ -62,9 +73,27 @@ public class TestJdbc {
 		
 	}
 	
+	public static Properties readPropertiesFile(String fileName) throws IOException{
+		Properties prop = null;
+		FileInputStream fis = null;
+		
+		try {
+			fis = new FileInputStream(fileName);
+			prop = new Properties();
+			prop.load(fis);
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			fis.close();
+		}
+		return prop;
+	}
+	
 	public static void createArticle(Article article) {
 		String create = "INSERT INTO T_Articles (Description, Brand, UnitaryPrice, IdCategory) VALUES (?,?,?,?);";
-		try(Connection connection = DriverManager.getConnection(url, login, password)){
+		try(Connection connection = DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.login"), prop.getProperty("db.password"))){
 			try(PreparedStatement ps = connection.prepareStatement(create)) {
 				ps.setString(1, article.getDescription());
 				ps.setString(2, article.getBrand());
@@ -80,7 +109,7 @@ public class TestJdbc {
 	
 	public static void updateArticle() {
 		String update = "UPDATE T_Articles SET Brand = ? WHERE IdArticle = ?";
-		try(Connection connection = DriverManager.getConnection(url, login, password)){
+		try(Connection connection = DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.login"), prop.getProperty("db.password"))){
 			try(PreparedStatement ps = connection.prepareStatement(update)){
 				ps.setString(1, "Avast");
 				ps.setInt(2, 15);
@@ -94,7 +123,7 @@ public class TestJdbc {
 	
 	public static void removeArticle(Article article) {
 		String remove = "DELETE FROM T_Articles WHERE IdArticle = ?";
-		try(Connection connection = DriverManager.getConnection(url, login, password)){
+		try(Connection connection = DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.login"), prop.getProperty("db.password"))){
 			try(PreparedStatement ps = connection.prepareStatement(remove)){
 				ps.setInt(1, article.getIdArticle());
 				if(ps.executeUpdate() == 1)
@@ -107,7 +136,7 @@ public class TestJdbc {
 	
 	public static void showInfoArticle(Article article) {
 		String showInfo = "SELECT IdArticle, Description, Brand, UnitaryPrice, IdCategory FROM T_Articles WHERE IdArticle = ?";
-		try(Connection connection = DriverManager.getConnection(url, login, password)){
+		try(Connection connection = DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.login"), prop.getProperty("db.password"))){
 			try(PreparedStatement ps = connection.prepareStatement(showInfo)){
 				ps.setInt(1, article.getIdArticle());
 				if(ps.executeUpdate() == 1) {
