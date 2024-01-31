963faaeeeb7ab6d6c56c2fbe24acdf87fb22b07c
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-31 14:05:31 +0100

    Add Logger to Dao

--- src/fr/fms/dao/Dao.java
+++ src/fr/fms/dao/Dao.java
@@ -2,9 +2,11 @@ package fr.fms.dao;
 
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public interface Dao<T> {
 
+	public static final Logger logger = Logger.getLogger(Dao.class.getName());
 	public Connection connection = BddConnection.getConnection();
 	public void create(T obj);
 	public T read(int id);
