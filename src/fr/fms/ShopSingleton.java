2ee46cb004a4905b8ef48fd49ba0c70e6f147d0d
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-01 13:48:47 +0100

    Add Singleton for one instance

---
+++ src/fr/fms/ShopSingleton.java
@@ -0,0 +1,39 @@
+package fr.fms;
+
+import fr.fms.dao.ArticleDao;
+import fr.fms.dao.UserDao;
+
+public class ShopSingleton {
+
+	private static ShopSingleton INSTANCE = null;
+	
+	private ArticleDao articles;
+	private UserDao users;
+
+	private ShopSingleton() {
+
+		
+	}
+	
+	public static ShopSingleton getInstance() {
+		if(INSTANCE == null) {
+			synchronized(ShopSingleton.class) {
+				if(INSTANCE == null) {
+					INSTANCE = new ShopSingleton();
+					INSTANCE.articles = new ArticleDao();
+					INSTANCE.users = new UserDao();
+				}
+			}
+		}
+		return INSTANCE;
+	}
+
+	public ArticleDao getArticles() {
+		return articles;
+	}
+
+	public UserDao getUsers() {
+		return users;
+	}
+	
+}
